b37269e32b0bac231fc535bf50cc66691e671d85
POOJA15GOYAL <poojagoyal1506@gmail.com>
2024-08-28 17:08:48 +0530

    register controller fixed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,7 @@
 import {asyncHandler} from "../utils/asyncHandler.js"
 import {Apierror} from "../utils/Apierror.js"
 import {User} from "../models/user.model.js"
-import uplaodOnCloudinary from "../utils/cloudinary.js"
+import {uplaodOnCloudinary} from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 
 
@@ -17,14 +17,19 @@ const registerUser=asyncHandler(async(req,res)=>{
 throw new Apierror(400,"all these fields are required")
     }
 
-    const existedUser= User.findOne({
+    const existedUser= await User.findOne({
         $or :[{username},{email}]
     })
     if(existedUser){
         throw new Apierror(409,"user with email or username already exists")
     }
+    console.log(req.files)
+    
     const avatarLocalPath=req.files?.avatar[0]?.path;
-    const coverImageLocalPath=req.files?.coverImage[0]?.path;
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }    
     if(!avatarLocalPath){
         throw new Apierror(400,"avatar file is required")
     }
@@ -35,7 +40,7 @@ throw new Apierror(400,"all these fields are required")
         throw new Apierror(400,"avatar file required")
     }
 
-    const user=awaitUser.create({
+    const user= await User.create({
         fullname,
         avatar:avatar.url,
         coverImage:coverImage?.url|| "" ,
